6c66e0bc297c0f66f9ed6a230efd6733f405fd83
Kane Yang <kay.627@qq.com>
2026-05-08 18:02:38 +0800

    Add conditional import for IPv6 support

--- SJBaseVideoPlayer/Common/Implements/SJReachability.m
+++ SJBaseVideoPlayer/Common/Implements/SJReachability.m
@@ -90,7 +90,9 @@ typedef void (^NetworkUnreachable) (_Reachability * reachability);
 
 #import <sys/socket.h>
 #import <netinet/in.h>
+#if !__has_feature(modules) && __has_include(<netinet6/in6.h>)
 #import <netinet6/in6.h>
+#endif
 #import <arpa/inet.h>
 #import <ifaddrs.h>
 #import <netdb.h>
